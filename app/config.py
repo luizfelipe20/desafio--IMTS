affd8eef94ae82d7cd0710dea8d70855f0faf252
luizfelipe20 <felipekjs36@gmail.com>
2019-08-23 13:25:25 +0000

    configurando o banco de dados

--- app/config.py
+++ app/config.py
@@ -1,19 +1,13 @@
-"""
-Classe exemplo de Configurações Básicas do Servidor
-"""
-import os
+APP_NAME = "Desafio IMTS"
+DEBUG = True 
 
-class BaseConfig(object):
-    APP_NAME = "Desafio IMTS"
-    DEBUG = True 
-
-    DB_DRIVER = os.getenv('DB_DRIVER', 'postgresql')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '5432')
-    DB_DATABASE = os.getenv('DB_DATABASE', '') # Redefinir
-    DB_USER = os.getenv('DB_USER', '') # Redefinir
-    DB_USER_PASSWORD = os.getenv('DB_PASSWORD', '') # Redefinir
+DB_DRIVER = 'postgresql'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_DATABASE = 'postgres' # Redefinir
+DB_USER = 'postgres' # Redefinir
+DB_USER_PASSWORD = '85334206' # Redefinir
     
-    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'\
-        .format(DB_DRIVER, DB_USER, DB_USER_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'\
+    .format(DB_DRIVER, DB_USER, DB_USER_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = True
