aceb98aba223a3d4ff33367ea00a8e611c039125
luizfelipe20 <felipekjs36@gmail.com>
2019-08-23 22:18:24 +0000

    testando os templates

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,17 @@
 
 def configure(app):
+	
 	@app.route("/index")
 	@app.route("/")
 	def index():
 	    return "Hello World!", 200
+
+
+	@app.route("/langs")
+	def langs():
+		langs = ["Python", "PHP", "JS"]
+		return render_template(
+			'index.html',
+			title="Melhores Linguagens"
+			linguagens=langs
+		)
