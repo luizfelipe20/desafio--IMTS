99462a0b6a8b15d782f8e5b97aa91b5e9ecd961a
luizfelipe20 <felipekjs36@gmail.com>
2019-08-24 14:41:01 +0000

    configurando um banco nosql

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import contact
+import db
 # import admin
 from app.controllers import default
 from flask import Flask
@@ -6,8 +7,21 @@ from flask import Flask
 
 def create_app():
 	# app = Flask(__name__, template_folder='/foo/bar', static_folder='/foo/bar')
+	# criacao do app
 	app = Flask(__name__)
+
+	
+	#conexao com o banco
+	db.configure(app)
+
+	
+	#view default para teste
 	default.configure(app)
+	
+
 	# admin.configure(app)
+	
+
+	#formulario de contato
 	contact.configure(app)
 	return app
